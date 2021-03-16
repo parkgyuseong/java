65d92c9e1de50100c9c63a13403b640b36ddcd3e
jaedeok-han <dgd03023@cu.ac.kr>
2021-03-16 15:36:48 +0900

    dsd

---
+++ src/operator/Operator2.java
@@ -0,0 +1,17 @@
+package operator;
+
+public class Operator2 {
+
+	public static void main(String[] args) {
+
+		int gameScore = 150;
+		
+		int lastScore1 = ++gameScore;
+		System.out.println(lastScore1);
+		
+		int lastScore2 = --gameScore;
+		System.out.println(lastScore2);
+		
+	}
+
+}
